19d85b79e45021ef2fa2cd9a83fba96e7129b481
Hatuma <hatumacharles1@gmail.com>
2026-03-09 12:58:09 +0200

    Update URLs and templates for machine learning sections

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,8 +3,7 @@ from predictor import views
 
 urlpatterns = [
     path("data_exploration", views.data_exploration_view, name="data_exploration"),
-    # Adding stubs so template reverse url doesn't fail
     path("regression_analysis", views.regression_analysis, name="regression_analysis"),
-    path("classification_analysis", views.data_exploration_view, name="classification_analysis"),
-    path("clustering_analysis", views.data_exploration_view, name="clustering_analysis"),
+    path("classification_analysis", views.classification_analysis, name="classification_analysis"),
+    path("clustering_analysis", views.clustering_analysis, name="clustering_analysis"),
 ]
